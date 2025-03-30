6c285f889df4db2b1bea540da45b11b7d3a56e87
Harriet-J <seremharriet@gmail.com>
2025-03-30 17:58:13 +0000

    fixed arrow error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,8 @@
-# Increases the amount of traffic an Nginx server can handle.
-
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
+} -> exec { 'nginx-restart':  # 👈 Fix: Arrow now correctly links both blocks
   command => 'nginx restart',
   path    => '/etc/init.d/'
 }
